6fccd7e6e1193a08c96db5e317a15d44b5f98245
yui-yoshizawa <yui.yossy.0114@gmail.com>
2021-02-11 08:12:00 +0900

    [Add] ViewBinding

--- app/src/main/java/com/example/marubatuapp/MainActivity.kt
+++ app/src/main/java/com/example/marubatuapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.marubatuapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.marubatuapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
