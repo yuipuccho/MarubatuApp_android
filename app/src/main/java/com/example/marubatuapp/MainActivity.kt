3682b2cd16b78ded416be44eabe450df57323233
yui-yoshizawa <yui.yossy.0114@gmail.com>
2021-02-11 12:05:44 +0900

    [Add] Script

--- app/src/main/java/com/example/marubatuapp/MainActivity.kt
+++ app/src/main/java/com/example/marubatuapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.marubatuapp
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.os.Bundle
 import com.example.marubatuapp.databinding.ActivityMainBinding
 
@@ -13,6 +14,9 @@ class MainActivity : Activity() {
 
     /** 現在何問目か */
     private var currentQuestionNum = 1
+
+    /** ヒントを見ることができるか */
+    private var canShowHint: Boolean = true
 
     private enum class Questions(val id: Int, val question: Int, val answer: Boolean) {
         Q1(1, R.string.question1, false),
@@ -29,14 +33,15 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // バインディング
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        println(Questions.Q1.id)
-        println(Questions.getQuestionFromId(1).id)
-        println(Questions.values().size)
-        println("aaa")
+        // ボタンタップ
+        binding.hintButton.setOnClickListener { tappedHintButton() }
+        binding.maruButton.setOnClickListener { tappedAnswerButton(true) }
+        binding.batuButton.setOnClickListener { tappedAnswerButton(false) }
 
         showQuestion()
     }
@@ -45,6 +50,65 @@ class MainActivity : Activity() {
     private fun showQuestion() {
         if (currentQuestionNum > Questions.values().size) { return }
         binding.questionText.setText(Questions.getQuestionFromId(currentQuestionNum).question)
+    }
+
+    /** 回答ボタンタップ */
+    private fun tappedAnswerButton(tappedButton: Boolean) {
+        var alertMessage: String
+        val nextQuestionNum = currentQuestionNum + 1
+
+        val result = judgeAnswer(tappedButton)
+        alertMessage = if (result) getString(R.string.feedback_correct) else getString(R.string.feedback_mistake)
+
+        // 次の問題があるか
+        if (nextQuestionNum <= Questions.values().size) {
+            // 次の問題がある
+            currentQuestionNum = nextQuestionNum
+        } else {
+            // 次の問題がない
+            alertMessage += getString(R.string.result_text, Questions.values().size, correctCount)
+            clear()
+        }
+        showAlert(alertMessage)
+        showQuestion()
+    }
+
+    /** 答え合わせ */
+    private fun judgeAnswer(tappedButton: Boolean): Boolean {
+        var result: Boolean = (tappedButton == Questions.getQuestionFromId(currentQuestionNum).answer)
+        if (result) correctCount += 1
+        return result
+    }
+
+    /** ヒントボタンタップ */
+    private fun tappedHintButton() {
+        var alertMessage: String
+
+        if (canShowHint) {
+            // ヒントを見ることができる場合
+            canShowHint = false
+            alertMessage = if (Questions.getQuestionFromId(currentQuestionNum).answer) getString(R.string.hint_maru) else getString(R.string.hint_batu)
+        } else {
+            // ヒントを見ることができない場合
+            alertMessage = getString(R.string.hint_cannot_show)
+        }
+
+        showAlert(alertMessage)
+    }
+
+    /** アラート表示 */
+    private fun showAlert(message: String) {
+        AlertDialog.Builder(this)
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
+    }
+
+    /** 初期化 */
+    private fun clear() {
+        canShowHint = true
+        currentQuestionNum = 1
+        correctCount = 0
     }
 
 }
